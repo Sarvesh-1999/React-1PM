9871c8a9c9a40c12440401c0ae193c0e2c1e0da4
Sarvesh <>
2026-06-08 14:39:19 +0530

    Fetch Api

--- src/AllTopics/09_FetchApi/ProductsList.jsx
+++ src/AllTopics/09_FetchApi/ProductsList.jsx
@@ -1,11 +1,15 @@
-const ProductsList = () => {
+import { useState } from "react";
 
-    // API CALL
+const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+
+  // API CALL
   const handleFetch = async () => {
     try {
       let resp = await fetch("https://dummyjson.com/products");
       let data = await resp.json();
-      console.log(data.products);
+      console.log(data.products); // [{},{},...]
+      setProducts(data.products); // [{},{},...]
     } catch (error) {
       console.log(error);
     }
@@ -15,6 +19,23 @@ const ProductsList = () => {
     <div>
       <h1>Learn Fetch Api in React</h1>
       <button onClick={handleFetch}>Fetch</button>
+
+      {products.length === 0 ? (
+        <p>No products available</p>
+      ) : (
+        <section>
+          {products.map((item) => {
+            return (
+              <div key={item.id}>
+                <img src={item.thumbnail} alt={item.title} height={300} width={300} />
+                <h3>{item.title}</h3>
+                <p>{item.description.slice(0,50)}...</p>
+                <button>Add to Cart</button>
+              </div>
+            );
+          })}
+        </section>
+      )}
     </div>
   );
 };
---
+++ src/AllTopics/09_FetchApi/TodosList.jsx
@@ -0,0 +1,11 @@
+const TodosList = () => {
+  return (
+    <div>
+      <h1>Fetch TodosAPI and Display on UI</h1>
+    </div>
+  );
+};
+export default TodosList;
+
+
+// https://dummyjson.com/todos
